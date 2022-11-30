940cb2474de4a68ec6db3a906efab3a9ba257d04
Matthes-Baer <Matthes.baer@yahoo.de>
2022-11-30 11:47:41 +0100

    feat: Loop through SingleItem component

--- src/components/nutrition/NutritionOverview.vue
+++ src/components/nutrition/NutritionOverview.vue
@@ -1,6 +1,7 @@
 <script lang="ts">
 import { store } from "../../store/store";
 import items from "../../data/items";
+import SingleItem from "./SingleItem.vue";
 
 export default {
   data() {
@@ -10,13 +11,22 @@ export default {
     };
   },
   props: [],
-  components: {},
+  components: {
+    SingleItem,
+  },
   methods: {},
   mounted() {},
 };
 </script>
 
 <template>
-  <div v-for="item in items" :key="item.id">{{ item.name }}</div>
-  <div v-for="item in store.itemsData" :key="item.id">{{ item.name }}</div>
+  <!-- <div v-for="item in items" :key="item.id">{{ item.name }}</div> -->
+  <div>
+    <!-- To loop through data with a component the loop has to be integrated in the component while the items within the loop have to be integrated as props in the respective component - v-bind: is identical to : -->
+    <SingleItem
+      v-for="item in store.itemsData"
+      :key="item.id"
+      v-bind:product="item"
+    />
+  </div>
 </template>
---
+++ src/components/nutrition/SingleItem.vue
@@ -0,0 +1,19 @@
+<script lang="ts">
+import { store } from "../../store/store";
+export default {
+  data() {
+    return {
+      store,
+    };
+  },
+  props: ["product"],
+  components: {},
+  methods: {},
+  mounted() {
+    console.log(this.product);
+  },
+};
+</script>
+<template>
+  <div>{{ product.name }}</div>
+</template>
