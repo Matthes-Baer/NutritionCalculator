d54edc5d032987d4fcd2eab64d615162976dbe99
Matthes-Baer <Matthes.baer@yahoo.de>
2023-01-06 02:45:03 +0100

    updated items.ts

--- src/data/items.ts
+++ src/data/items.ts
@@ -10,12 +10,6 @@ const DATA: {
     icon: "https://www.cheeselinks.com.au/wp-content/uploads/2017/09/Dollarphotoclub_53674521.jpg",
     values: { kcal: 2, fat: 0.2, sugar: 0.01, proteine: 0.15 },
   },
-  {
-    id: 2,
-    name: "Tomato",
-    icon: "https://www.schoolphotoproject.com/_picture-of-vegetable/tomato-photo2-l.jpg",
-    values: { kcal: 0.02, fat: 0.001, sugar: 0.001, proteine: 0.001 },
-  },
 ];
 
 export default DATA;
