9eb5c26e9f47f7f004abb801e274bfa22c448001
Matthes-Baer <Matthes.baer@yahoo.de>
2023-01-09 23:11:59 +0100

    feat: Updated style on headercomp

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,11 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from "vue-router";
+import { RouterView } from "vue-router";
+import HeaderComp from "./components/HeaderComp.vue";
 </script>
 
 <template>
   <div>
-    <nav>
-      <RouterLink to="/">Home</RouterLink>
-      <RouterLink to="/about">About</RouterLink>
-    </nav>
+    <HeaderComp />
     <RouterView />
   </div>
 </template>
---
+++ src/components/HeaderComp.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import { RouterLink } from "vue-router";
+</script>
+
+<template>
+  <nav class="header-container">
+    <RouterLink to="/" class="header-item">Home</RouterLink>
+    <RouterLink to="/about" class="header-item">About</RouterLink>
+  </nav>
+</template>
+
+<style scoped lang="scss">
+.header {
+  &-container {
+    display: flex;
+    flex-direction: row;
+    justify-content: end;
+    margin-top: 1%;
+  }
+  &-item {
+    margin-right: 1%;
+  }
+}
+
+@media (max-width: 480px) {
+  .header {
+    &-container {
+      flex-direction: column;
+      align-items: center;
+    }
+    &-item {
+      margin-right: 0;
+      margin-bottom: 1%;
+    }
+  }
+}
+</style>
