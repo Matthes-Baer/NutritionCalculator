7aec458ef0f890e0244e8a5ba095fda7dd421085
Matthes-Baer <Matthes.baer@yahoo.de>
2023-01-11 21:16:41 +0100

    feat: Added links in footer component

--- src/components/FooterComp.vue
+++ src/components/FooterComp.vue
@@ -4,15 +4,27 @@ export default {};
 <template>
   <div class="main-container d-flex justify-content-center align-items-end">
     <div class="d-flex align-items-center me-3">
-      <i class="fa-brands fa-linkedin icon"></i>
+      <a
+        href="https://www.linkedin.com/in/matthes-b%C3%A4r-68199a256/"
+        target="_blank"
+      >
+        <i class="fa-brands fa-linkedin icon"></i>
+      </a>
     </div>
 
     <div class="d-flex align-items-center me-3">
-      <i class="fa-solid fa-newspaper icon"></i>
+      <a
+        href="https://www.freecodecamp.org/news/author/matthes/"
+        target="_blank"
+      >
+        <i class="fa-solid fa-newspaper icon"></i>
+      </a>
     </div>
 
     <div class="d-flex align-items-center">
-      <i class="fa-brands fa-github icon"></i>
+      <a href="https://github.com/Matthes-Baer" target="_blank">
+        <i class="fa-brands fa-github icon"></i
+      ></a>
     </div>
   </div>
 
@@ -25,6 +37,19 @@ export default {};
 <style scoped lang="scss">
 :root {
   --icon-font-size: 50px;
+}
+
+a:visited {
+  text-decoration: none;
+}
+
+a:hover {
+  opacity: 0.5;
+}
+
+a {
+  cursor: pointer;
+  color: white;
 }
 .main-container {
   width: 100%;
