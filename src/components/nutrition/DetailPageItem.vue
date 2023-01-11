67a5367b11254677d6988622ab857d2c65b3a9be
Matthes-Baer <Matthes.baer@yahoo.de>
2023-01-11 21:56:41 +0100

    feat: Updated DetailsPageIem

--- src/components/nutrition/DetailPageItem.vue
+++ src/components/nutrition/DetailPageItem.vue
@@ -1,6 +1,7 @@
 <script lang="ts">
 import router from "@/router";
 import { findSpecificItem } from "@/data/items";
+import DetailsItemNutrition from "./Details/DetailsItemNutrition.vue";
 
 export default {
   data() {
@@ -15,11 +16,62 @@ export default {
       return new URL(`../../assets/images/${name}`, import.meta.url).href;
     },
   },
+  components: { DetailsItemNutrition },
 };
 </script>
 
 <template>
-  <div v-if="specificItem?.image">
-    <img :src="getImageUrl(specificItem.image)" />
+  <div class="row p-3">
+    <div class="col-lg-12">
+      <h1>Details Page</h1>
+    </div>
+    <div class="col-lg-12">
+      <div class="d-flex align-items-center">
+        <div>Name:</div>
+        <div>
+          {{ specificItem?.name }}
+        </div>
+      </div>
+      <div>
+        <div>Nutrition Values (per 100g)</div>
+        <div class="d-flex flex-column nutrition-values-container">
+          <DetailsItemNutrition :nutritionValues="specificItem" type="kcal" />
+          <DetailsItemNutrition
+            :nutritionValues="specificItem"
+            type="fat"
+            unit="g"
+          />
+          <DetailsItemNutrition
+            :nutritionValues="specificItem"
+            type="carbs"
+            unit="g"
+          />
+          <DetailsItemNutrition
+            :nutritionValues="specificItem"
+            type="protein"
+            unit="g"
+          />
+        </div>
+      </div>
+    </div>
+    <div v-if="specificItem?.image" class="img-container">
+      <img :src="getImageUrl(specificItem.image)" />
+    </div>
   </div>
 </template>
+
+<style scoped lang="scss">
+img {
+  width: 100%;
+  height: auto;
+}
+
+.img-container {
+  width: 10%;
+}
+
+.nutrition-values-container {
+  width: 25%;
+  min-width: 250px;
+}
+</style>
---
+++ src/components/nutrition/Details/DetailsItemNutrition.vue
@@ -0,0 +1,15 @@
+<script lang="ts">
+export default {
+  data() {
+    return {};
+  },
+  props: ["nutritionValues", "type", "unit"],
+};
+</script>
+
+<template>
+  <div class="d-flex justify-content-between">
+    <div>{{ type }}:</div>
+    <div>{{ nutritionValues?.values[type] }}{{ unit }}</div>
+  </div>
+</template>
